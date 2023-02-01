da6570a419d768a2dd45afc566d2d6885cda499b
hsjun1996 <hsjun1996@naver.com>
2023-02-02 03:06:50 +0900

    Fix : 반응형 헤더 수정

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,38 +1,31 @@
-import { useState } from "react";
+import MenuIcon from "@mui/icons-material/Menu";
 import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
 const Header = () => {
-  return (
-    <>
-      <nav className={styles.navbar}>
-        <h1 className={styles.logo}>
-          <Link to="#">SSAVALON</Link>
-        </h1>
-        <div className={`${styles.navbar_container} ${styles.container}`}>
-          <input type="checkbox" name="" id="" />
-          <div className={styles.hamburger_lines}>
-            <span className={`${styles.line} ${styles.line1}`}></span>
-            <span className={`${styles.line} ${styles.line2}`}></span>
-            <span className={`${styles.line} ${styles.line3}`}></span>
-          </div>
-          <ul className={styles.menu_items}>
-            <li>
-              <Link to="#">로그인</Link>
-            </li>
-            <li>
-              <Link to="#">로그아웃</Link>
-            </li>
-            <li>
-              <Link to="#">로비</Link>
-            </li>
-            <li className={styles.btn}>
-              <Link to="#">마이페이지</Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </>
-  );
+    return (
+        <>
+            <nav className={styles.nav}>
+                <label className={styles.logo}>SSAVALON</label>
+                <input type="checkbox" id="check" className={styles.check} />
+                <label htmlFor="check" className={styles.checkbtn}>
+                    <MenuIcon />
+                </label>
+                <ul>
+                    <li>
+                        <Link to="#" className={styles.active}>
+                            로그아웃
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="#">로비</Link>
+                    </li>
+                    <li>
+                        <Link to="#">마이페이지</Link>
+                    </li>
+                </ul>
+            </nav>
+        </>
+    );
 };
 
 export default Header;
