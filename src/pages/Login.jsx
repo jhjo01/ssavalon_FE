e22f5fea9453285226e8021f89b19ff0f27efd48
조재형 <whwogud0301@naver.com>
2023-02-01 17:59:39 +0900

    ADD : kakao login

---
+++ src/pages/Login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/header/Header";
+import LoginPage from "../components/login/Login";
+
+const Login = () => {
+  return (
+    <>
+      <Header />
+      <LoginPage />
+    </>
+  );
+};
+
+export default Login;
