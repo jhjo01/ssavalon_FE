ca9a2459b4539d7b9c6a33c6ac3c2e9560c64fa4
한상준 <hsjun1996@naver.com>
2023-02-02 15:18:12 +0900

    Style : mypage design

--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -1,9 +1,10 @@
 import Header from "../components/header/Header";
-
+import MypageInfo from "../components/mypage/MypageInfo";
 const Mypage = () => {
   return (
     <>
       <Header />
+      <MypageInfo />
     </>
   );
 };
