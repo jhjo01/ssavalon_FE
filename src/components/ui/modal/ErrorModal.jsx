d0eca7c096390ce526eaf6f45f1c4bd0fcf35f9c
hsjun1996 <hsjun1996@naver.com>
2023-02-04 12:36:37 +0900

    Modify : 폴더 이름 변경

--- src/components/ui/modal/ErrorModal.jsx
+++ src/components/ui/modal/ErrorModal.jsx
@@ -1,45 +1,47 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import ButtonPrimary from "../Button/ButtonPrimary";
+import ButtonPrimary from "../button/ButtonPrimary";
 import Backdrop from "./Backdrop";
 
 import styles from "./Modal.module.css";
 
 const ModalOverlay = (props) => {
-  const title = "입장 에러";
-  const message = "인원이 가득찼습니다.";
+    const title = "입장 에러";
+    const message = "인원이 가득찼습니다.";
 
-  return (
-    <div className={styles.modal}>
-      <div className={styles.card}>
-        <header className={styles.header}>
-          <h2>{title}</h2>
-        </header>
-        <div className={styles.content}>
-          <p>{message}</p>
+    return (
+        <div className={styles.modal}>
+            <div className={styles.card}>
+                <header className={styles.header}>
+                    <h2>{title}</h2>
+                </header>
+                <div className={styles.content}>
+                    <p>{message}</p>
+                </div>
+                <footer className={styles.actions}>
+                    <ButtonPrimary onClick={props.onConfirm}>
+                        확인
+                    </ButtonPrimary>
+                </footer>
+            </div>
         </div>
-        <footer className={styles.actions}>
-          <ButtonPrimary onClick={props.onConfirm}>확인</ButtonPrimary>
-        </footer>
-      </div>
-    </div>
-  );
+    );
 };
 
 const ErrorModal = (props) => {
-  return (
-    <React.Fragment>
-      {ReactDOM.createPortal(
-        <Backdrop onConfirm={props.onConfirm} />,
-        document.getElementById("backdrop-root")
-      )}
-      {ReactDOM.createPortal(
-        <ModalOverlay onConfirm={props.onConfirm} />,
-        document.getElementById("overlay-root")
-      )}
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            {ReactDOM.createPortal(
+                <Backdrop onConfirm={props.onConfirm} />,
+                document.getElementById("backdrop-root")
+            )}
+            {ReactDOM.createPortal(
+                <ModalOverlay onConfirm={props.onConfirm} />,
+                document.getElementById("overlay-root")
+            )}
+        </React.Fragment>
+    );
 };
 
 export default ErrorModal;
