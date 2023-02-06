32899a26ead59fb12bf361b1662d505167d14ccd
한상준 <hsjun1996@naver.com>
2023-02-06 12:19:39 +0900

    Fix : Merge Conflice

--- src/App.js
+++ src/App.js
@@ -4,19 +4,18 @@ import Game from "./pages/Game";
 import Index from "./pages/Index";
 import Lobby from "./pages/LobbyPage";
 import Mypage from "./pages/Mypage";
-import GamePage from "./pages/GamePage";
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Index />} />
-                <Route path="/lobby" element={<Lobby />} />
-                <Route path="/mypage" element={<Mypage />} />
-                <Route path="/game" element={<Game />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/lobby" element={<Lobby />} />
+        <Route path="/mypage" element={<Mypage />} />
+        <Route path="/game" element={<Game />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
