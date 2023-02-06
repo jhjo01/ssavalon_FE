c7f21344ef56b08f949934f3506a3e1cfcf8711d
sangzun-han <hsjun1996@naver.com>
2023-02-06 21:37:08 +0900

    Style : card-back design

--- src/components/ui/gameboard/GameBoard.jsx
+++ src/components/ui/gameboard/GameBoard.jsx
@@ -2,7 +2,6 @@ import styles from "./GameBoard.module.css";
 import GameBoardImage from "../../../assets/images/image-game-board.png";
 import AvatarImage from "../avatar/AvatarImage";
 import ButtonRS from "../button/ButtonRS";
-import HalfCircle from "../under-card/HalfCircle";
 
 const GameBoard = () => {
   const peoples = [
