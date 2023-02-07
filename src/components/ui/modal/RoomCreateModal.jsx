8ec9b54ab45207b73c8bddd66ab64b94bf5f6dda
sangzun-han <hsjun1996@naver.com>
2023-02-07 23:47:40 +0900

    Fix : Merge Confilct

--- src/components/ui/modal/RoomCreateModal.jsx
+++ src/components/ui/modal/RoomCreateModal.jsx
@@ -12,121 +12,110 @@ import { API_END_POINT } from "../../../constants";
 // import { createRoom } from "../../../hooks/createRoom";
 
 const ModalOverlay = (props) => {
-    const [lock, setLock] = useState(false);
-    const [err, setErr] = useState(null);
+  const [lock, setLock] = useState(false);
+  const [err, setErr] = useState(null);
 
-    const setLockHandler = () => {
-        setLock(!lock);
-    };
+  const setLockHandler = () => {
+    setLock(!lock);
+  };
 
-    const setErrHandler = (props) => {
-        setErr(props);
-    };
+  const setErrHandler = (props) => {
+    setErr(props);
+  };
 
-    const title = "방만들기";
+  const title = "방만들기";
 
-    const titleInputRef = useRef();
-    const pwdInputRef = useRef();
+  const titleInputRef = useRef();
+  const pwdInputRef = useRef();
 
-    const createRoomHandler = (event) => {
-        event.preventDefault();
-        const enteredTitle = titleInputRef.current.value;
-        if (enteredTitle.length < 1) {
-            setErrHandler("title");
-            return;
-        }
+  const createRoomHandler = (event) => {
+    event.preventDefault();
+    const enteredTitle = titleInputRef.current.value;
+    if (enteredTitle.length < 1) {
+      setErrHandler("title");
+      return;
+    }
 
-        if (lock) {
-            const enteredPwd = pwdInputRef.current.value;
-            if (enteredPwd.trim().length < 6) {
-                setErrHandler("pwd");
-                return;
-            }
-            // createRoom("/game/room", { enteredTitle });
-            
-            titleInputRef.current.value = "";
-            
-        }
+    if (lock) {
+      const enteredPwd = pwdInputRef.current.value;
+      if (enteredPwd.trim().length < 6) {
+        setErrHandler("pwd");
+        return;
+      }
+      // createRoom("/game/room", { enteredTitle });
 
-        const createUrl = API_END_POINT + "/game/room";
+      titleInputRef.current.value = "";
+    }
 
-        const form = new FormData()
-        form.append('name', enteredTitle)
+    const createUrl = API_END_POINT + "/game/room";
 
-        axios.post(createUrl, form)
-        .then((res) => {
-            console.log(res);
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-        // createRoom("/game/room", { enteredTitle});
-        titleInputRef.current.value = "";
+    const form = new FormData();
+    form.append("name", enteredTitle);
 
-        props.onConfirm();
-    };
+    axios
+      .post(createUrl, form)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    // createRoom("/game/room", { enteredTitle});
+    titleInputRef.current.value = "";
 
+    props.onConfirm();
+  };
 
-    return (
-        <div className={styles.modal}>
-            <div className={styles.card}>
-                <header className={styles.header}>
-                    <h2>{title}</h2>
-                </header>
-                <div className={styles.card}>
-                    <form className={styles.form} onSubmit={createRoomHandler}>
-                        <label htmlFor="roomtitle">방제목</label>
-                        <input
-                            className={styles.input}
-                            id="roomtitle"
-                            type="text"
-                            ref={titleInputRef}
-                        />
-                        {err === "title" ? (
-                            <p className={styles.inputErrMsg}>
-                                방 이름은 x글자 이상 x글자 이하로 설정해주세요.
-                            </p>
-                        ) : (
-                            <br />
-                        )}
+  return (
+    <div className={styles.modal}>
+      <div className={styles.card}>
+        <header className={styles.header}>
+          <h2>{title}</h2>
+        </header>
+        <div className={styles.card}>
+          <form className={styles.form} onSubmit={createRoomHandler}>
+            <label htmlFor="roomtitle">방제목</label>
+            <input
+              className={styles.input}
+              id="roomtitle"
+              type="text"
+              ref={titleInputRef}
+            />
+            {err === "title" ? (
+              <p className={styles.inputErrMsg}>
+                방 이름은 x글자 이상 x글자 이하로 설정해주세요.
+              </p>
+            ) : (
+              <br />
+            )}
 
-                        {lock && (
-                            <div>
-                                <label htmlFor="pwd">비밀번호</label>
-                                <input
-                                    className={styles.input}
-                                    id="pwd"
-                                    type="password"
-                                    ref={pwdInputRef}
-                                />
-                                {err === "pwd" && (
-                                    <p className={styles.inputErrMsg}>
-                                        방 비밀번호는 x글자 이상 x글자 이하로
-                                        설정해주세요.
-                                    </p>
-                                )}
-                            </div>
-                        )}
-                        <label>
-                            <input
-                                type="checkbox"
-                                name="gender"
-                                onClick={setLockHandler}
-                            />
-                            비밀방
-                        </label>
-                        <footer className={styles.actions}>
-                            <ButtonPrimary type="submit">만들기</ButtonPrimary>
-                            <ButtonDanger
-                                type="reset"
-                                onClick={props.onConfirm}
-                            >
-                                취소
-                            </ButtonDanger>
-                        </footer>
-                    </form>
-                </div>
-            </div>
+            {lock && (
+              <div>
+                <label htmlFor="pwd">비밀번호</label>
+                <input
+                  className={styles.input}
+                  id="pwd"
+                  type="password"
+                  ref={pwdInputRef}
+                />
+                {err === "pwd" && (
+                  <p className={styles.inputErrMsg}>
+                    방 비밀번호는 x글자 이상 x글자 이하로 설정해주세요.
+                  </p>
+                )}
+              </div>
+            )}
+            <label>
+              <input type="checkbox" name="gender" onClick={setLockHandler} />
+              비밀방
+            </label>
+            <footer className={styles.actions}>
+              <ButtonPrimary type="submit">만들기</ButtonPrimary>
+              <ButtonDanger type="reset" onClick={props.onConfirm}>
+                취소
+              </ButtonDanger>
+            </footer>
+          </form>
         </div>
       </div>
     </div>
