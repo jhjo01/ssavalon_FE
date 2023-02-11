75fcfb0e81d07a8fed0b9e84fb966b3f4897b16d
sangzun-han <hsjun1996@naver.com>
2023-02-11 18:24:12 +0900

    Fix : https

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const API_END_POINT = "http://i8b305.p.ssafy.io:3030";
+export const API_END_POINT = "https://i8b305.p.ssafy.io:3030";
 export const OPENVIDU_SERVER_URL = "http://i8b305.p.ssafy.io:5000";
 export const OPENVIDU_SERVER_SECRET = "MY_SECRET";
 export const SOCKET_SUB_END_POINT = "/sub/message/user";
