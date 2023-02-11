884ec5d5b0fa82c427f2205af9640248bebe931b
sangzun-han <hsjun1996@naver.com>
2023-02-11 16:00:20 +0900

    Feat : card rotate

--- src/components/game/gameboard/GameBoard.jsx
+++ src/components/game/gameboard/GameBoard.jsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useSocket, ready } from "../../../hooks/useSocket";
 import { useParams } from "react-router-dom";
@@ -8,25 +8,37 @@ import AvatarImage from "../avatar/AvatarImage";
 import ButtonRS from "../../common/button/ButtonRS";
 import RoundLog from "../logCard/RoundLog";
 import { selectorRoomAndStandBy } from "./../../../store/roomAndStandBy";
-import {
-  selectorRoomAndActive,
-  updateGameState,
-} from "./../../../store/roomAndActive";
+import { updateGameState } from "./../../../store/roomAndActive";
 import UnderCard from "../underCard/UnderCard";
-import { CSSTransition } from "react-transition-group";
-
 const GameBoard = () => {
-  /*
-    TwoChoice를 위한 코드
-  */
+  const [modalOpen, setModalOpen] = useState(false);
 
-  const gameStatus = useSelector(selectorRoomAndActive);
   const dispatch = useDispatch();
 
-  const test = () => {
+  const open = () => {
+    setModalOpen(true);
     dispatch(
       updateGameState({
-        status: "voteAgreeDisgree",
+        status: "voteAgreeDisagree",
+        roomId: "",
+        connectedUsers:
+          '[{"userId": cici, "userNickName": cici, "job":"", "isLeader": cici, "isJury: cici}]',
+        round: "",
+        voteRound: "",
+        prevRound: '[{"round": 0, "win":cici}]',
+        agreeDisagree: '[{"userId": cici, "userNickName":cici, "agree": cici}]',
+        guilty: "2",
+        notGuilty: "1",
+        script: "asd",
+      })
+    );
+  };
+
+  const close = () => {
+    setModalOpen(false);
+    dispatch(
+      updateGameState({
+        status: "",
         roomId: "",
         connectedUsers:
           '[{"userId": cici, "userNickName": cici, "job":"", "isLeader": cici, "isJury: cici}]',
@@ -69,20 +81,11 @@ const GameBoard = () => {
           <ButtonRS content="준비" onClick={() => sendMessage("READY")} />
           <ButtonRS content="나가기" />
         </div>
-        <button onClick={test}>adas</button>
+        <button onClick={open}>열기</button>
+        <button onClick={close}>닫기</button>
       </div>
 
-      <CSSTransition
-        in={gameStatus.status === "voteAgreeDisgree"}
-        classNames={styles.alert}
-        timeout={1000}
-      >
-        <UnderCard title="agree" />
-      </CSSTransition>
-
-      {gameStatus.status === "voteGuiltyNotGuilty" && (
-        <UnderCard title="guilty" />
-      )}
+      <UnderCard open={modalOpen} />
       <RoundLog round={1} />
       <RoundLog round={2} />
     </>
--- src/components/game/underCard/UnderCard.jsx
+++ src/components/game/underCard/UnderCard.jsx
@@ -4,21 +4,35 @@ import { useSelector } from "react-redux";
 import { selectorRoomAndActive } from "./../../../store/roomAndActive";
 
 const UnderCard = (props) => {
-  const { title } = props;
-  const [agree, setAgree] = useState([]);
-  const gameStatus = useSelector(selectorRoomAndActive);
-  const cardUp =
-    gameStatus.status === "voteAgreeDisgree" ||
-    gameStatus.status === "voteGuiltyNotGuilty"
-      ? styles.half_circle_up
-      : "";
+  const { open } = props;
+  const [animate, setAnimate] = useState(false);
+  const [visible, setVisible] = useState(open);
 
+  const gameStatus = useSelector(selectorRoomAndActive);
+  const [agree, setAgree] = useState([]);
+
+  console.log(open);
+  const title = gameStatus.status === "voteAgreeDisagree" ? "agree" : "guilty";
   useEffect(() => {
     title === "agree" ? setAgree(["찬성", "반대"]) : setAgree(["무죄", "유죄"]);
   }, [title]);
 
+  useEffect(() => {
+    if (visible && !open) {
+      setAnimate(true);
+      setTimeout(() => setAnimate(false), 500);
+    }
+    setVisible(open);
+  }, [visible, open]);
+
+  if (!animate && !visible) return null;
   return (
-    <div className={`${styles.half_circle} ${cardUp}`}>
+    <div
+      className={`${open ? styles.half_circle_up : styles.half_circle_down} ${
+        styles.half_circle
+      }`}
+      tabIndex={-1}
+    >
       <div className={styles.card_agree}>
         <div className={styles.card_content_agree}>
           <p>{agree[0]}</p>
