87f619e43bfb9887dce6004fc3cc188d3de02454
조재형 <whwogud0301@naver.com>
2023-02-15 15:57:10 +0900

    Update src/constants/index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,6 +5,6 @@ export const API_BUSINESS = "https://i8b305.p.ssafy.io:8000";
 export const API_SOCKET = "http://i8b305.p.ssafy.io:9001";
 
 // Kakao API_KEY, REDIRECT_URL
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URL}&response_type=code`;
 const REST_API_KEY = "e21948e5e842e29a34dd1216456745dd";
 const REDIRECT_URL = "http://localhost:3000/oauth/kakao";
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URL}&response_type=code`;
