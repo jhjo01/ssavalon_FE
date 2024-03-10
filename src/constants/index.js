1aec3cd28b2c2bc5dc6870e38f8c55346e88cdc9
jhjo01 <109667442+jhjo01@users.noreply.github.com>
2024-03-10 20:19:44 +0900

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,11 +4,11 @@ export const CHAT_SUB_END_POINT = "/sub/chat/user";
 export const CHAT_PUB_END_POINT = "/pub/chat/user";
 export const GAME_SUB_END_POINT = "/sub/message/user";
 export const GAME_PUB_END_POINT = "/pub/message/user";
-export const API_BUSINESS = "https://i8b305.p.ssafy.io:8000";
-export const API_END_POINT = "http://i8b305.p.ssafy.io:9002";
-export const API_SOCKET = "http://i8b305.p.ssafy.io:9001";
+export const API_BUSINESS = "";
+export const API_END_POINT = "";
+export const API_SOCKET = "";
 
 // Kakao API_KEY, REDIRECT_URL
-const REST_API_KEY = "e21948e5e842e29a34dd1216456745dd";
-const REDIRECT_URL = "http://localhost:3000/oauth/kakao";
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URL}&response_type=code`;
+const REST_API_KEY = "";
+const REDIRECT_URL = "";
+export const KAKAO_AUTH_URL = ``;
